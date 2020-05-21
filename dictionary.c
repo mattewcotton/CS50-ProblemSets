ec32ceb2b56375defcbc6424091160779be873b5
Matthew Cotton <61757779+matthewcotton@users.noreply.github.com>
2020-05-21 11:22:31 -0400

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -20,7 +20,7 @@ node;
 unsigned int dicSize = 0;
 
 // Number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 5490;
 
 // Hash table
 node *table[N];
@@ -48,67 +48,15 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    char c = tolower(word[0]);
-    //Implement a basic hash table based on the first char of the word
-    switch (c)
-    {
-        case 'a':
-            return 0;
-        case 'b':
-            return 1;
-        case 'c':
-            return 2;
-        case 'd':
-            return 3;
-        case 'e':
-            return 4;
-        case 'f':
-            return 5;
-        case 'g':
-            return 6;
-        case 'h':
-            return 7;
-        case 'i':
-            return 8;
-        case 'j':
-            return 9;
-        case 'k':
-            return 10;
-        case 'l':
-            return 11;
-        case 'm':
-            return 12;
-        case 'n':
-            return 13;
-        case 'o':
-            return 14;
-        case 'p':
-            return 15;
-        case 'q':
-            return 16;
-        case 'r':
-            return 17;
-        case 's':
-            return 18;
-        case 't':
-            return 19;
-        case 'u':
-            return 20;
-        case 'v':
-            return 21;
-        case 'w':
-            return 22;
-        case 'x':
-            return 23;
-        case 'y':
-            return 24;
-        case 'z':
-            return 25;
-        default:
-            return 0;
-    }
+    unsigned int hash = 0;
+    unsigned int length = strlen(word);
 
-    return 0;
+    for (int i = 0; i < length; i++)
+    {
+        char c = tolower(word[i]);
+        hash += c;
+    }
+    return hash;
 }
 
 // Loads dictionary into memory, returning true if successful else false
